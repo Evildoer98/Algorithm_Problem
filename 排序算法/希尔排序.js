156487335767945bd9001ec7fe3f4aaf91835c21
Evildoer98 <1213089093@qq.com>
2021-05-07 23:28:46 +0800

    v3

---
+++ 排序算法/希尔排序.js
@@ -0,0 +1,27 @@
+/**
+ * 
+ * 插入序列
+ *   1. 原理：插入排序的工作原理是通过构建有序序列，对于未排序数据，
+ *           在已排序序列中从后向前扫描，找到相应位置并插入
+ *   
+ *   2. 核心思想：取未排序区间中的元素，在已排序区间中找到合适的插入位置将其插入，
+ *               并保证已排序区间数据一直有序。重复这个过程，直到未排序区间中元素为空 
+ * 
+ * 时间复杂度：O(n^2)
+ * 
+ */
+// 插入排序
+function insertSort (arr) {
+    let len = arr.length
+    let preIndex, current
+    for (let i = 1; i < len; i++) {
+        preIndex = i - 1
+        current = arr[i]
+        while (preIndex >= 0 && arr[preIndex] > current) {
+            arr[preIndex + 1] = arr[preIndex]
+            preIndex--
+        }
+        arr[preIndex + 1] = current
+    }
+    return arr
+}
--- 排序算法/打乱数组(洗牌算法).js
+++ 排序算法/打乱数组(洗牌算法).js
@@ -4,16 +4,18 @@
 
 /**
  * 浅拷贝数组，利用 random() 方法重置数组下标
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(n)
  */
-
+// 保存原数组
 var Solution = function (nums) {
     this.nums = nums
 }
-
+// 重置原数组
 Solution.prototype.reset = function () {
     return this.nums
 }
-
+// 打乱数组
 Solution.prototype.shuffle = function () {
     let num = this.nums.slice()
     for (let i = 0; i < nums.length; i++) {
@@ -22,4 +24,30 @@ Solution.prototype.shuffle = function () {
     }
     return num
 }
+
+// 法二
+function solution1 (arr) {
+    let tempArr = arr.slice()
+    for (let i = 0; i < tempArr.length; i++) {
+        let mark = Math.floor(Math.random() * (tempArr.length - i) + i)
+        let temp = tempArr[i]
+        tempArr = tempArr[mark]
+        tempArr[mark] = temp
+    }
+    return tempArr
+}
+
+/**
+ * sort(() => Math.random() - 0.5)
+ * 
+ * sort() 会根据 compareFunction(a, b) 的返回值，来决定 a 和 b 的相对位置
+ * 如果 compareFunction(a, b) 小于 0，那么 a 会被排列到 b 之前
+ * 如果 compareFunction(a, b) 大于 0，那么 a 会被排列到 a 之前
+ * 如果 compareFunction(a, b) 等于 0，那么 a 和 b 的相对位置不变
+ * 
+ * 真正的乱排序
+ * 最后一位开始向前遍历，每次循环生成随机数
+ * Math.random() * (i+1)，向下取整，然后两个位置元素交换
+ * 
+ */
 
