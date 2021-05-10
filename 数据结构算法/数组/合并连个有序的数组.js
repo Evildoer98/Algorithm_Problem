76d052bda305e365323d030e22e33d44a739d012
Evildoer98 <1213089093@qq.com>
2021-05-10 23:09:44 +0800

    v6

--- 数据结构算法/数组/合并连个有序的数组.js
+++ 数据结构算法/数组/合并连个有序的数组.js
@@ -13,75 +13,85 @@
  * 输出: [1,2,2,3,5,6]
  * 
  */
-// 时间复杂度 O(n+m)
+var nums1 = [1,2,3]
+var nums2 = [0,2,5,6]
+
+// 法一：调用 api
 /**
- * 
- *  nums1 和 nums2 有序，若把 nums2 全部合并到 nums1，则合并后的 nums1 长度为 m + n
- *  
- *  从下标 m + n - 1 的位置填充 nums1，比较 nums1[len1] 与 nums2[len2] 的大小，将最大值写入 nums[len]，即
- *    1. nums1[len1] >= nums2[len2] , nums[len--] = nums1[len1--] , 这里的 -- 是因为写入成功，下标自动减一，继续向前比较
- *    2. 否则 nums[len--] = nums2[len2--]
- *  边界条件：
- *    1. 若 len1 < 0，即 len2 >= 0，此时 nums1 已重写入，nums2 还未合并完，仅仅需要将 nums2 的剩余元素 （0...len）写入 nums2 即可，写入后，合并完成
- *    2. 若 len2 < 0，此时 nums2 已全部合并到 nums1，合并完成
- * 
+ * 其中 sort 在 Chorme 中使用的快排 时间复杂度为 O(nlogn)
  */
-const merge = function(nums1, m, nums2, n){
-    let len1 = m -1
+var merge1 = function (nums1, nums2) {
+    var result = []
+    result = nums1.concat(nums2)
+    return result.sort((a, b) => a - b)
+}
+console.log(merge1(nums1, nums2)) // [0, 0, 0, 1, 2, 2, 3, 5, 6]
+
+// 法二
+/**
+ * filter() 方法创建一个新的数组，新数组中的元素是通过检查指定数组中符合条件的所有元素。
+ * 自动将 0 去掉了
+ */
+var merge2 = function (nums1, nums2) {
+    if (!(nums1 instanceof Array)) {
+        nums1 = []
+    }
+    if (!(nums2 instanceof Array)) {
+        nums2 = []
+    }
+    return nums1.concat(nums2).filter(item => item).sort((a, b) => a - b);
+}
+console.log(merge2(nums1, nums2)) // [1, 2, 2, 3, 5, 6]
+
+// 法三
+var merge3 = function (nums1, m, nums2, n) {
+    let len1 = m - 1
     let len2 = n - 1
-    let len = m + n - 1 
-    while (len2 > 0) {
+    let len = m + n - 1
+    while (len2 >= 0) {
         if (len1 < 0) {
-            num1[len--] = num2[len2--]
+            nums1[len--] = nums2[len2--]
             continue
         }
         nums1[len--] = nums1[len1] >= nums2[len2] ? nums1[len1--] : nums2[len2--]
     }
+    return nums1
 }
-
+var m = 3
+var n = 4
+console.log(merge3(nums1, m, nums2, n));
 // 不改变原数组
-const merge2 = function(nums1,nums2){
-    var len1 = nums1.length - 1
-    var len2 = nums2.length - 1
-    var len = nums1.length + nums2.length - 1
-    var [...nums] = nums1 
+var merge3_1 = function (nums1, nums2) {
+    let len1 = nums1.length - 1
+    let len2 = nums2.length - 1
+    let len = nums1.length + nums2.length - 1
+    let [...nums] = nums1
     while (len2 >= 0) {
         if (len1 < 0) {
             nums[len--] = nums2[len2--]
+            continue        
         }
         nums[len--] = nums1[len1] >= nums2[len2] ? nums1[len1--] : nums2[len2--]
-    } 
+    }
     return nums
 }
+console.log(merge3_1(nums1, nums2))
 
-// 调用 api 
+// 法四
 /**
- * sort ： O(nlogn)
+ * shift() 方法用于把数组的第一个元素从其中删除，并返回第一个元素的值
  */
-const merge3 = function (nums1, nums2) {
-    var arr = nums1.concat(nums2)
-    return arr.sort((a, b) => a - b)
-}
-
-// 归并排序---该算法采用的分治法
-const merge4 = function (nums1, nums2) {
-    var result = []
-    while (nums1.length > 0 && nums2.length > 0) {
-        if (nums1[0] < nums2[0]) {
-            result.push(nums1.shift())
-        } else {
-            result.push(nums2.shift())  
-        }
-    }    
-    return result.concat(nums1).concat(nums2)
-}
-
-const mergeSort = function (array) {
-    if (array.length === 1) {
-        return array
+var merge3 = function (nums1, nums2) {
+    let result = []
+    while (nums1.length && nums2.length) {
+        result.push(nums1[0] <= nums2[0] ? nums1.shift() : nums2.shift())
     }
-    var middle = Math.floor(array.length / 2)
-    var left = array.slice(0, middle)
-    var right = array.slice(middle)
-    return merge4(mergeSort(left), mergeSort(right))
+    return result
 }
+console.log(merge3(nums1, nums2));
+
+// 法五
+/**
+ * 归并排序
+ */
+
