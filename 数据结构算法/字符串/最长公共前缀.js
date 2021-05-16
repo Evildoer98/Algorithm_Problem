1af62126062370983d525ade49ad09c43ce19398
Evildoer98 <1213089093@qq.com>
2021-05-16 22:46:43 +0800

    v13

--- 数据结构算法/字符串/最长公共前缀.js
+++ 数据结构算法/字符串/最长公共前缀.js
@@ -1,20 +1,36 @@
 /**
- * 查找字符串数组中的最长公共前缀
+ * 查找字符串数组中的最长公共前缀,如果不存在公共前缀，返回空字符串 ""
+ * 
  * 输入：["flower","flow","flight"]
  * 输出：["fl"]
+ * 
+ * 输入: ["dog","racecar","car"]
+ * 输出: ""
+ * 解释: 输入不存在公共前缀
+ * 
  */
+
+var strs1 = ["flower","flow","flight"]
+var strs2 = ["dog","racecar","car"]
+
 // 法一：逐个比较
-const f1 = function(strs){
+/**
+ * 时间复杂度 O(s) , s 是所有字符串中字符数量的总和
+ * 空间复杂度 O(1)
+ */
+const f = function (strs) {
     if (strs === null || strs.length === 0) {
         return ""
     }
     let pre = strs[0]
-    for (let i = 1; i < strs.length; i++) {
+    let len = strs.length
+    for (let i = 1; i < len; i++) {
         let j = 0
-        for (; j < strs.length && j < pre.length; j++) {
+        while (j < strs.length && j < pre.length) {
             if (strs[i].charAt(j) !== pre.charAt(j)) {
                 break
-            }   
+            }
+            j++
         }
         pre = pre.substring(0, j)
         if (pre === "") {
@@ -24,8 +40,60 @@ const f1 = function(strs){
     return pre
 }
 
+const f1 = function(strs){
+    if (strs === null || strs.length === 0) {
+        return ""
+    }
+    let pre = strs[0]
+    for (let i = 1; i < strs.length; i++) {  
+        for (var j = 0; j < strs.length && j < pre.length; j++) {
+            if (strs[i].charAt(j) !== pre.charAt(j)) {
+                break
+            }
+        }
+        pre = pre.substring(0, j)
+        if (pre === "") {
+            return ""
+        }
+    }
+    return pre
+}
+console.log(f(strs1));
+console.log(f(strs2));
+
+console.log(f1(strs1));
+console.log(f1(strs2));
+
+
 // 法二：仅需最大和最小字符串的最长公共前缀
-const f2 = function(strs){
+const f2 = function (strs) {
+    let len = strs.length
+    if (strs === null || len === 0) {
+        return ""
+    }
+    if (len === 1) {
+        return strs[0]
+    }
+    let min = 0
+    let max = 0
+    for (let i = 0; i < len; i++) {
+        if (strs[min] > strs[i]) {
+            min = i
+        }
+        if (strs[max] < strs[i]) {
+            max = i
+        }
+    }
+    for (let j = 0; j < strs[min].length; j++) {
+        // charAt() 方法可返回指定位置的字符。
+        if (strs[min].charAt(j) !== strs[max].charAt(j)) {
+            return strs[min].substring(0, j)
+        } 
+    }
+    return strs[min]
+}
+
+const f3 = function(strs){
     if (strs.length === 0) {
         return ''
     }
@@ -34,6 +102,7 @@ const f2 = function(strs){
     }
     // 使用sort是按照ASCII码表一个一个的排序出来的字符串数组，所以前面的几乎都一样了
     strs = strs.sort()
+    // console.log(strs);
     let result = ''
     let start = strs[0]
     let end = strs[strs.length - 1]
@@ -50,6 +119,82 @@ const f2 = function(strs){
     return result
 }
 
-var strs = ["flower","flow","flight"]
-console.log(f1(strs));
-console.log(f2(strs));
+console.log(f2(strs1));
+console.log(f2(strs2));
+
+console.log(f3(strs1));
+console.log(f3(strs2));
+
+// 法三：分治策略，归并思想
+/**
+ * 分治，顾名思义，就是分而治之，将一个复杂的问题，分成两个或多个相似的子问题，
+ * 再把子问题分成更小的子问题，直到更小的子问题可以简单求解，求解子问题，
+ * 则原问题的解则是子问题解的合并
+ * 
+ * 问题：多个字符串的最长公共前缀
+ * 分解成多个相似的子问题：求两个字符串的最长公共前缀
+ * 原问题的解为子问题的合并：多个字符串的最长公共前缀为两两字符串的最长公共前缀的最长公共前缀，
+ * 我们可以归并比较两最长公共前缀字符串的最长公共前缀，直到最后归并比较成一个，则为字符串数组的最长公共前缀
+ * 
+ * LCP(S1, S2, ... ,Sn) = LCP(LCP(S1, Sk), LCP(Sk+1, Sn))
+ * 
+ */
+
+const f4 = function (strs) {
+    if (strs === null || strs.length === 0) {
+        return ""
+    }
+    return LCP(strs)
+}
+/**
+ * 若分裂后的两个数组长度不为 1，则继续分裂
+ * 直到分裂后的数组长度都为 1
+ * 然后比较获取最长公共前缀
+ */
+function LCP (strs) {
+    let len = strs.length
+    if (len === 1) {
+        return strs[0]
+    }
+    let mid = Math.floor(len / 2)
+    let left = strs.slice(0, mid)
+    let right = strs.slice(mid, length - 1)
+    return LCPfixTwo(LCP(left), LCP(right))
+}
+function LCPfixTwo (str1, str2) {
+    let i = 0
+    while (i < str1.length && i < str2.length) {
+        if (str1.charAt(i) !== str2.charAt(i)) {
+            break
+        }
+        i++
+    }
+    return str1.substring(0, i)
+}
+console.log(f4(strs1));
+console.log(f4(strs2));
+
+// 法四：树（字典树）
+/**
+ * 构建一颗树，字符串数组的最长公共序列就为从根节点开始遍历树，直到：
+ *  遍历节点存在超过一个子节点的节点
+ *  或者遍历节点一个字符串的结束字符
+ * 为止，走过的字符为字符串数组的最长公共前缀
+ *                  f
+ *                 /
+ *                l
+ *               /\
+ *              o  i
+ *             /    \
+ *            w      g
+ *           /        \
+ *          e          t
+ *         /
+ *        r
+ * 
+ * 取最长公共位置 fl
+ */
+
+const f5 = function (strs) {
+    
+}
--- 数据结构算法/字符串/翻转字符串的单词.js
+++ 数据结构算法/字符串/翻转字符串的单词.js
@@ -2,28 +2,54 @@
  * 给定一个字符串，逐个反转字符串中的每个单词
  * 输入："the sky is bule"
  * 输出："bule is sky the"
+ * 
+ * 输入: "  hello world!  "
+ * 输出: "world! hello"
+ * 解释: 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+ * 
+ * 输入: "a good   example"
+ * 输出: "example good a"
+ * 解释: 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+ * 
+ * 说明：
+ * 无空格字符构成一个单词。
+ * 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+ * 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
  */
+var s1 = "the sky is bule"
+var s2 = "  hello world!  "
+var s3 = "a good   example"
 // 法一、正则 + JS API
 var f1 = function(s){
     // trim() 删除空格
     return s.trim().replace(/\s+/g, ' ').split(' ').reverse().join(' ')
 }
-
+console.log(f1(s1));
+console.log(f1(s2));
+console.log(f1(s3));
 // 法二、双端队列
+/**
+ * 双端队列：两端都可以进队的队列
+ * 
+ * 思路：
+ *  首先去除字符串左右空格
+ *  逐个读取字符串中的每个单词，依次放入双端队列的队头
+ *  再将队列转换成字符串输出（已空格为分隔符）
+ */
 var f2 = function(s){
     let left = 0
     let right = s.length - 1
     let queue = []
     let word = ''
     // charAt() 方法用于返回指定索引处的字符
-    // while (s.charAt(left) === ' ') {
-    //     left++
+    while (s.charAt(left) === ' ') {
+        left++
         
-    // }
+    }
     // console.log(left);
-    // while (s.charAt(right) === ' ') {
-    //     right--
-    // }
+    while (s.charAt(right) === ' ') {
+        right--
+    }
     // console.log(right);
     while (left <= right) {
         let char = s.charAt(left)
@@ -41,7 +67,9 @@ var f2 = function(s){
     // 元素是通过指定的分隔符进行分隔的。
     return queue.join(' ')
 }
-
+console.log(f2(s1));
+console.log(f2(s2));
+console.log(f2(s3));
 // 法三、手动反转
 var f3 = function(s){
     // filter(Boolean) 移除空格
@@ -54,7 +82,6 @@ var f3 = function(s){
     }
     return newArr.join(' ')
 }
-var s = "the sky is bule"
-// console.log(f1(s));
-console.log(f2(s));
-console.log(f3(s));
+console.log(f3(s1));
+console.log(f3(s2));
+console.log(f3(s3));
